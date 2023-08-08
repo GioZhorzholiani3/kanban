5c623eccb4b70a2a3fe8a1a3ba9ac0abbdb86c26
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-08-08 21:07:37 +0400

    add dummy addTaks fn

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,26 +2,38 @@
 import { useStore } from "../store/store";
 import classes from "./Column.module.css";
 import Task from "./Task";
-import { shallow } from "zustand/shallow";
+import { useState } from "react";
+// import { shallow } from "zustand/shallow";
 
 type ColumnProps = {
   state: string;
 };
 
 const Column: React.FC<ColumnProps> = (props) => {
-  const tasks = useStore(
-    (store) => store.tasks.filter((task) => task.state === props.state),
-    shallow
+  const tasks = useStore((store) =>
+    store.tasks.filter((task) => task.state === props.state)
   );
 
+  const addTask = useStore((store) => store.addTask);
   // const filtered = useMemo(
   //   () => tasks.filter((task) => task.status === props.state),
   //   [tasks, props.state]
   // );
 
+  const [text, setText] = useState("");
+
   return (
     <div className={classes.column}>
-      <p>{props.state}</p>
+      <div className={classes.titleWrapper}>
+        <p className={classes.state}>{props.state}</p>
+        <button
+          onClick={() => {
+            addTask("asd" + props.state, props.state);
+          }}
+        >
+          Add
+        </button>
+      </div>
       {tasks.map((task) => (
         <Task key={task.title} title={task.title} />
       ))}
